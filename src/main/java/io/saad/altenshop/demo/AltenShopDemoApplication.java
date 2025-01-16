d7c9a7e6b2bc9e7146c94e4b328f49b78f80025a
Saad ELALLAM <saadelallam92@gmail.com>
2025-01-16 05:17:14 +0100

    initial code

---
+++ src/main/java/io/saad/altenshop/demo/AltenShopDemoApplication.java
@@ -0,0 +1,13 @@
+package io.saad.altenshop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltenShopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltenShopDemoApplication.class, args);
+	}
+
+}
